fe76e298fc6b1f9dd9a5f37e7e39d6f60971e7e6
Ethan Curtis <95894855+UltraSpeedDemon@users.noreply.github.com>
2024-11-04 18:43:09 -0500

    Task 1 Completed

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,30 @@
+/* Programming Languages - Assignment 2
+*  Ethan Curtis - 1166648
+*  ############ - #######
+*
+* Practices Functional Programming in Kotlin
+* */
+
+// Define small functions as defined in Task 1
+fun addTen(number: Int):
+        Int = number + 10 // Adds 10 to the number
+fun multiplyByThree(number: Int):
+        Int = number * 3 // Multiplies the number by 3
+fun isGreaterThanFifty(number: Int):
+        Boolean = number > 50 // Returns true if the number is greater than 50
+
+//apply these functions to a list of numbers using a single variable
+val processNumber: (Int) -> Boolean = { isGreaterThanFifty(multiplyByThree(addTen(it))) }
+
 fun main() {
-    println("Hello World!")
+    // list of numbers to test on
+    val numbers = listOf(3, 16, 22, 37, 43)
+
+    // variable of the results
+    val results = numbers.map(processNumber)
+
+    //skip line
+    println()
+    // Printing results
+    println("Results for each number in the list: $results") // Example output
 }
